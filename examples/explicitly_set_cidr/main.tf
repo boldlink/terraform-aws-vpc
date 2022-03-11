f98d72c6ead16fe5f6cbdb71e2a9adbf88a9829a
patrick <mukumbu.katiwa@boldlink.io>
2022-03-11 20:48:54 +0300

    NOSTORY: added ipam cidr

--- examples/explicitly_set_cidr/main.tf
+++ examples/explicitly_set_cidr/main.tf
@@ -1,5 +1,5 @@
 module "aws_vpc" {
-  source               = "./.."
+  source               = "./../.."
   cidr_block           = "172.16.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
